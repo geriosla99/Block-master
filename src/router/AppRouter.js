2fa4770fd4862699f9ddd0200f41189a21db9fa7
geriosla99 <geriosla@gmail.com>
2021-07-25 18:25:01 -0500

    creates routes of register and login

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LogIn from "../components/LogIn";
 import Register from "../components/Register";
+import Movies from "../containers/Movies";
 
 export const AppRouter = () => {
     return (
         <div>
             <Router>
                 <Switch>
-                    <Route exact path='/' component={LogIn}/>
+                    <Route exact path='/' component={Movies}/>
+                    <Route exact path='/login' component={LogIn}/>
                     <Route exact path='/registro' component={Register}/>
                 </Switch>
             </Router>
