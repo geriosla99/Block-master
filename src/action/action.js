d2bc89b1bbb9e10ed5ae1260a7aa3135c26c5774
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-27 09:26:01 -0500

    Correcciones

--- src/action/action.js
+++ src/action/action.js
@@ -1,58 +1,57 @@
-import {types} from '../types/types'
-import { firebase,google } from "../firebase/firebaseConfig";
+import { types } from '../types/types'
+import { firebase, google } from "../firebase/firebaseConfig";
 
-export const registroEmailPasswordName = (email, pass, name) =>{
-    return(dispatch)=>{
-        firebase.auth().createUserWithEmailAndPassword(email,pass)
-        .then( async({user})=>{
-            console.log(user);
+export const login = (id, displayName) => {
+    return {
+        type: types.login,
+        payload: {
+            id,
+            displayName
+        }
+    }
+}
 
-            await user.updateProfile({displayName: name})
+export const loginGoogle = () => {
+    return (dispatch) => {
+        firebase.auth().signInWithPopup(google)
+            .then(({ user }) => {
+                console.log(user);
+                dispatch(
+                    login(user.uid, user.displayName))
+            })
+    }
+}
 
-            dispatch(
-                login(user.uid,user.displayName)
-            )
-        })
-        .catch(e =>{
-            console.log(e);
-        })
+export const registroEmailPasswordName = (email, pass, name) => {
+    return (dispatch) => {
+        firebase.auth().createUserWithEmailAndPassword(email, pass)
+            .then(async ({ user }) => {
+                console.log(user);
+
+                await user.updateProfile({ displayName: name })
+
+                dispatch(
+                    login(user.uid, user.displayName)
+                )
+            })
+            .catch(e => {
+                console.log(e);
+            })
     }
 }
 
 //primera funcion asincrona
-export const loginEmailPassword = (email,password) =>{
+export const loginEmailPassword = (email, password) => {
     //devuelve un callback
-    return(dispatch)=>{
-        firebase.auth().signInWithEmailAndPassword(email,password)
-        .then(({user}) => {
-            dispatch(
-                login(user.uid,user.displayName)
-            )
-        })
-        .catch(e =>{
-            console.log(e)
-        })
-    }
-}
-
-
-export const loginGoogle = () =>{
-    return (dispatch) =>{
-        firebase.auth().signInWithPopup(google)
-        .then(({user}) =>{
-            console.log(user);
-            dispatch(
-                login(user.uid,user.displayName))
-        })
-    }
-}
-
-export const  login = ( id, displayName ) =>{
-    return{
-        type: types.login,
-        payload:{
-            id,
-            displayName
-        }
+    return (dispatch) => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(({ user }) => {
+                dispatch(
+                    login(user.uid, user.displayName)
+                )
+            })
+            .catch(e => {
+                console.log(e)
+            })
     }
 }
