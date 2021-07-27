180b468ed081155d27d144962e79cba6692e2bb9
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-27 12:04:21 -0500

    Estilos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Search from "../Search/Search";
-import All from "../All/All";
-import MoreValued from "../MoreValue/MoreValue";
-import LessValued from "../LessValued/LessValued";
+import Search from '../Search/Search'
+import All from '../All/All'
+import MoreValued from '../MoreValue/MoreValue'
+import LessValued from '../LessValued/LessValued'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const NavbarBM = () => {
-    return (
-        <Navbar bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="#home">
-            <img
-                src="https://i.imgur.com/RJQjzuf.png"
-                width="106,81"
-                height="64"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-            />
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand href='#home'>
+          <img
+            src='https://i.imgur.com/RJQjzuf.png'
+            width='106,81'
+            height='64'
+            className='d-inline-block align-top'
+            alt='React Bootstrap logo'
+          />
         </Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link to="/"><All/></Nav.Link>
-            <Nav.Link to="/"><MoreValued/></Nav.Link>
-            <Nav.Link to="/"><LessValued/></Nav.Link>
-            <Nav.Link to="/"><Search/></Nav.Link>
-            <Nav.Link><Link to='/login'>
-                        Iniciar sesion
-                    </Link></Nav.Link>
-            </Nav>
-        </Container>
-      </Navbar>
-    )
+        <Nav className='me-auto'>
+          <Nav.Link to='/'>
+            <All />
+          </Nav.Link>
+          <Nav.Link to='/'>
+            <MoreValued />
+          </Nav.Link>
+          <Nav.Link to='/'>
+            <LessValued />
+          </Nav.Link>
+          <Nav.Link to='/'>
+            <Search />
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
 }
 
-export default NavbarBM;
+export default NavbarBM
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { ContainerSearch } from "./SearchStyled";
+import { ContainerSearch } from './SearchStyled'
+import { Form, FormControl, InputGroup } from 'react-bootstrap'
 
 const Search = () => {
-    return (
-        <ContainerSearch>
-            <input type='text'></input>
-        </ContainerSearch>
-    )
+  return (
+    <ContainerSearch>
+      <Form className='d-flex'>
+        <InputGroup className='mb-2'>
+          <FormControl
+            id='inlineFormInputGroup'
+            placeholder='Busca tu película favorita'
+          />
+          <InputGroup.Text>@</InputGroup.Text>
+        </InputGroup>
+      </Form>
+    </ContainerSearch>
+  )
 }
 
 export default Search
