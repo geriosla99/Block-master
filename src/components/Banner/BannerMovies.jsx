ffca2df42a83ecfdb0726e517a371b55e0895ead
geriosla99 <geriosla@gmail.com>
2021-07-27 19:08:06 -0500

    estilos del banner

--- src/components/Banner/BannerMovies.jsx
+++ src/components/Banner/BannerMovies.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
+import { Carousel, Button } from 'react-bootstrap'
+
 
 const BannerMovies = () => {
     return (
         <div>
-            aqui van los banner
+            <Carousel className='m-3'>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://i.imgur.com/gxyWIWg.png"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption className='d-flex flex-row'>
+                        <Button style={{background:'#FED941'}} className='d-flex me-3'>
+                            <img
+                            src="https://i.imgur.com/kSCBD6n.png"
+                            alt="First slide"
+                            class="align-middle"
+                            /> 
+                            <p className='text-center' style={{color:'#000000'}}>VER AHORA</p>
+                        </Button>
+                        <Button style={{background:'#0C0E16', color:'#FED941' }} className='d-flex me-3'>
+                            <img
+                            src="https://i.imgur.com/KW0i7zZ.png"
+                            alt="First slide"
+                            class="align-middle"
+                            /> 
+                            <p className='text-center'>VER DESPUÉS</p>
+                        </Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://i.imgur.com/6yZ0hND.png"
+                    alt="Second slide"
+                    />
+                    <Carousel.Caption className='d-flex flex-row'>
+                        <Button style={{background:'#FED941'}} className='d-flex me-3'>
+                            <img
+                            src="https://i.imgur.com/kSCBD6n.png"
+                            alt="First slide"
+                            /> 
+                            <p className='text-center' style={{color:'#000000'}}>VER AHORA</p>
+                        </Button>
+                        <Button style={{background:'#0C0E16', color:'#FED941' }} className='d-flex me-3'>
+                            <img
+                            src="https://i.imgur.com/KW0i7zZ.png"
+                            alt="First slide"
+                            /> 
+                            <p className='text-center'>VER DESPUÉS</p>
+                        </Button>
+                    </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.imgur.com/tHo7KLH.png"
+                        alt="Third slide"
+                        />
+                    <Carousel.Caption className='d-flex flex-row'> 
+                        <Button style={{background:'#FED941'}} className='d-flex me-3'>
+                                <img
+                                src="https://i.imgur.com/kSCBD6n.png"
+                                alt="First slide"
+                                /> 
+                                <p className='text-center' style={{color:'#000000'}}>VER AHORA</p>
+                            </Button>
+                            <Button style={{background:'#0C0E16', color:'#FED941' }} className='d-flex me-3'>
+                                <img
+                                src="https://i.imgur.com/KW0i7zZ.png"
+                                alt="First slide"
+                                /> 
+                                <p className='text-center'>VER DESPUÉS</p>
+                        </Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
